c36c62d7e1b240659222ad3b11da9a8e3d24f92d
morimonchan <morimorimonchan.com@gmail.com>
2022-10-13 16:30:02 +0900

    バリデーションの設定

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,5 +1,15 @@
 class Building < ApplicationRecord
 
+  with_options presence: true do
+
+  validates :building_name    
+  validates :address
+  validates :building_detail   
+  validates :user    
+  
+end
+
+  validates :place_id, numericality: { other_than: 1, message: "を入力してください" }          
 
   belongs_to :user
 
