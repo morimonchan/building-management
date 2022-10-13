3c0b3088637a994eb6fae93941466845793dff0f
morimonchan <morimorimonchan.com@gmail.com>
2022-10-13 23:45:23 +0900

    投稿画面、保存の設定

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -9,12 +9,21 @@ class BuildingsController < ApplicationController
     @building = Building.new
    end
  
-  #  def create
-  #    @building = Building.new(building_params)
-  #    if @building.save
-  #    redirect_to root_path
-  #    else
-  #    render :new
-  #    end
-  #  end
+   def create
+     @building = Building.new(building_params)
+     if @building.save
+     redirect_to root_path
+     else
+     render :new
+     end
+   end
+
+   private
+
+   def building_params
+     params.require(:building)
+           .permit(:building_name, :place_id, :address, :building_detail, :image)
+           .merge(user_id: current_user.id)
+    end
+
 end
--- app/models/building.rb
+++ app/models/building.rb
@@ -8,9 +8,12 @@ class Building < ApplicationRecord
   validates :building_detail   
   validates :user       
   end
-
   validates :place_id, numericality: { other_than: 1, message: "を入力してください" }          
 
   belongs_to :user
+
+  has_one_attached :image
+
+
 
 end
