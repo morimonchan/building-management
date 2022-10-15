4834f3ca053fe9060a778a3da3039ec5895ce9c6
morimonchan <morimorimonchan.com@gmail.com>
2022-10-15 16:21:13 +0900

    edit,updateアクションの設定

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -21,7 +21,26 @@ class BuildingsController < ApplicationController
    def show
     @building = Building.find(params[:id])
    end
+   
+   def edit
+    @building = Building.find(params[:id])
+   end
 
+   def update
+    @building = Building.find(params[:id])
+    @building.update(building_params)
+    if @building.save
+    redirect_to building_path,method: :get
+    else
+    render :edit
+    end
+  end
+
+   def destroy
+    @building = Building.find(params[:id])
+    @building.destroy
+    redirect_to root_path
+   end
 
    private
 
@@ -29,6 +48,6 @@ class BuildingsController < ApplicationController
      params.require(:building)
            .permit(:building_name, :place_id, :address, :building_detail, :image)
            .merge(user_id: current_user.id)
-    end
+   end
 
 end
